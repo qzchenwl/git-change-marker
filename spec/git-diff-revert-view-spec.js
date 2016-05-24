bed26abf4f38ced5696f0d98aee2efd675f6ba39
qzchenwl <qzchenwl@gmail.com>
2016-05-24 15:24:48 +0800

    init

---
+++ spec/git-diff-revert-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GitDiffRevertView from '../lib/git-diff-revert-view';
+
+describe('GitDiffRevertView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
