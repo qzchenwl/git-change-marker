c709014926b22205e7b06d1df8a66ad2f54dd984
chenwl <qzchenwl@gmail.com>
2016-05-27 11:32:26 +0800

    Delete git-diff-revert-view-spec.js

--- spec/git-diff-revert-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import GitDiffRevertView from '../lib/git-diff-revert-view';
-
-describe('GitDiffRevertView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
